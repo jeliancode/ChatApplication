899a858f51b579e8e20f309629dc8cb00789ceed
JelianCode <vgtmldnd@gmail.com>
2024-09-21 12:28:45 -0400

    Basic chat application

---
+++ BasicChatApp/ChatClient.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace BasicChatApp
+{
+    public class ChatClient
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 6000);
+                NetworkStream stream = client.GetStream();
+                Thread receiverThread = new Thread(() => ReceiveMesseges(stream));
+                receiverThread.Start();
+
+                while (true)
+                {
+                    string message = Console.ReadLine();
+                    byte[] buffer = Encoding.ASCII.GetBytes(message);
+                    stream.Write(buffer, 0, buffer.Length);
+                }
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+    }
+}
---
+++ BasicChatApp/ClientManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace BasicChatApp
+{
+    public class ClientManager
+    {
+        private NetworkStream senderStream;
+        private NetworkStream receiberStream;
+        private byte[] buffer;
+        private TcpClient _sender;
+        private TcpClient _receiber;
+
+        public void HandleClient(TcpClient sender, TcpClient receiber)
+        {
+            _sender = sender;
+            _receiber = receiber;
+            senderStream = _sender.GetStream();
+            receiberStream = _receiber.GetStream();
+            buffer = new byte[1024];
+
+            try
+            {
+                while (true)
+                {
+                    int byteCount = senderStream.Read(buffer, 0, buffer.Length);
+                    if (byteCount == 0)
+                    {
+                        break;
+                    }
+                    string message = Encoding.ASCII.GetString(buffer, 0, byteCount);
+                    Console.WriteLine("Mensaje recibido: " + message);
+                    receiberStream.Write(buffer, 0, byteCount);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error" + e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        private void CloseConnection()
+        {
+            senderStream.Close();
+            receiberStream.Close();
+            _sender.Close();
+            _receiber.Close();
+        }
+    }
+}
